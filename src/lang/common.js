4a01b3b3ae6c0e80cbaf27e5ece2ef59607a50f0
linlulu <2196601785@qq.com>
2020-09-15 15:21:10 +0800

    footer内容更新

--- src/lang/common.js
+++ src/lang/common.js
@@ -8,8 +8,8 @@ module.exports = {
         CENTERCONTENT: '作为一个开放式测试服务，集成构建&测试系统，软件调测、测试分析比较能力，旨在与社区开发者一起提升开源软件质量。'
     },
     FOOOTER: {
-        LEFTTITLE: 'Join our Crystal',
-        LEFTCONTENT: 'Sale up to 20% off for your next purchase in this month!',
+        LEFTTITLE: 'Join Crystal CI',
+        LEFTCONTENT: '让社区开发更简单',
         WORDLEFT: 'Enter your email address..',
         WORDRIGHT: 'Subscribe'
     }
