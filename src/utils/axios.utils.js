157b1c258f666232d4dfb211caabeb76fa67f894
linlulu <2196601785@qq.com>
2020-09-18 09:58:19 +0800

    正式端口

--- src/utils/axios.utils.js
+++ src/utils/axios.utils.js
@@ -3,8 +3,7 @@
  * */
 import axios from 'axios';
 import {Message, Loading} from 'element-ui';
-// const BASEURL = 'http://124.90.34.227:11320'; // 正式
-const BASEURL = 'http://124.90.34.227:11304';
+const BASEURL = 'http://124.90.34.227:11320'; 
 let loadingInstance = null;
 export const service = axios.create({
     timeout: 20 * 1000,
