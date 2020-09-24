b1a0b666a12ad1cd91464a2c2a6be77aee9f0808
linlulu <2196601785@qq.com>
2020-09-24 17:30:48 +0800

    规范描述

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import {Table, TableColumn, Pagination, Form, FormItem, Select, Option, Input, Cascader, Button, Tooltip} from 'element-ui';
+import {
+    Table,
+    TableColumn,
+    Pagination,
+    Form,
+    FormItem,
+    Select,
+    Option,
+    Input,
+    Cascader,
+    Button,
+    Tooltip
+} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
