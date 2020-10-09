13ac51178d37a2b229e10c6a81fbd78a6c456d4d
linlulu <2196601785@qq.com>
2020-10-09 10:18:02 +0800

    取消配置哦

--- vue.config.js
+++ vue.config.js
@@ -15,17 +15,3 @@ module.exports = {
         }
     }
 };
-let BASE_URL = ""; //这里是一个默认的url，可以没有
-switch (process.env.NODE_ENV) {
-    case 'development':
-        BASE_URL = "http://124.90.34.227:11304" //这里是本地的请求
-        break
-    case 'test':
-        BASE_URL = "http://124.90.34.227:11304" // 自定义的测试环境
-        break
-    case 'production':
-        BASE_URL = "https://api.compass-ci.openeuler.org:11320" //生产环境
-        break
-}
-
-export default BASE_URL;
