9849acdb4c6a54ec021a3a80bc43e7a4d2eabfa2
Li Yuanchao <lyc163mail@163.com>
2021-07-30 16:52:07 +0800

    调整表格列宽，改善表格显示
    Signed-off-by: Li Yuanchao <lyc163mail@163.com>

--- src/views/errorlist/errorlist.vue
+++ src/views/errorlist/errorlist.vue
@@ -5,11 +5,21 @@
     <div class="content">
       <h1 class="title">Error List</h1>
       <el-table :data="tableData" stripe>
-        <el-table-column  v-for="(item,index) in tableHead" :label="item" :prop="item" width="" :key="index">
-            <template slot-scope="scope">
-                <div class="go" v-if="item==='link_to_result'" @click="goBlank(scope.row[item])">result_root</div>
-                <div v-else>{{scope.row[item]}}</div>
-            </template>
+        <el-table-column
+          v-for="(item,index) in tableHead"
+          :label="item"
+          :prop="item"
+          :key="index"
+          :width="getWidth(index)"
+        >
+          <template slot-scope="scope">
+            <div
+              class="go"
+              v-if="item==='link_to_result'"
+              @click="goBlank(scope.row[item])"
+            >result_root</div>
+            <div v-else>{{scope.row[item]}}</div>
+          </template>
         </el-table-column>
       </el-table>
     </div>
@@ -21,37 +31,40 @@ import Header from "@/components/Header";
 export default {
   name: "ErrorList",
   components: {
-    Header,
+    Header
   },
   data() {
     return {
       tableHead: [],
       tableData: [],
       tableHeader: {},
-      jobsQuery: {},
+      jobsQuery: {}
     };
   },
   methods: {
-    goBlank(src){
-        window.open(src, "_blank"); 
-      },
-    getData(){
-        getErrorList(this.$route.query).then((res) => {
+    goBlank(src) {
+      window.open(src, "_blank");
+    },
+    getData() {
+      getErrorList(this.$route.query).then(res => {
         this.jobsQuery = res;
         this.tableHead = this.jobsQuery.attributes;
         this.tableData = this.jobsQuery.objects;
       });
     },
+    getWidth(index) {
+      return index % 3 == 0 ? 120 : 0;
+    }
   },
   mounted() {
     this.getData();
-  },
+  }
 };
 </script>
 <style lang='scss' scoped>
 .go {
-    cursor: pointer;
-    color:blue;
-    text-decoration: underline;
+  cursor: pointer;
+  color: blue;
+  text-decoration: underline;
 }
 </style>
