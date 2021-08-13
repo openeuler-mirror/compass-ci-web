f49d255090995d27bf50b388986a7f158b78929d
Li Yuanchao <lyc163mail@163.com>
2021-08-13 15:47:08 +0800

    调整表格列
    去掉error_id，合并job_id和result_root
    
    Signed-off-by: Li Yuanchao <lyc163mail@163.com>

--- src/views/errorlist/errorlist.vue
+++ src/views/errorlist/errorlist.vue
@@ -5,25 +5,25 @@
     <div class="content">
       <h1 class="title">Error List</h1>
       <el-table :data="tableData" stripe>
-        <el-table-column
-          v-for="(item,index) in tableHead"
-          :label="item"
-          :prop="item"
-          :key="index"
-          :width="getWidth(index)"
-        >
-          <template slot-scope="scope">
-            <div
-              class="go"
-              v-if="item==='result_root'"
-              @click="goBlank(scope.row[item])"
-            >result_root</div>
-
-            <div class="go" v-else-if="item==='stderr'" @click="goBlank(scope.row[item])">stderr</div>
-
-            <div v-else>{{scope.row[item]}}</div>
-          </template>
-        </el-table-column>
+        <template v-for="(item,index) in tableHead">
+          <el-table-column
+            v-if="item!='result_root'"
+            :label="item"
+            :prop="item"
+            :key="index"
+            :width="getWidth(index)"
+          >
+            <template slot-scope="scope">
+              <div
+                class="go"
+                v-if="item==='job_id'"
+                @click="goBlank(scope.row['result_root'])"
+              >{{scope.row[item]}}</div>
+              <div class="go" v-else-if="item==='stderr'" @click="goBlank(scope.row[item])">stderr</div>
+              <div v-else>{{scope.row[item]}}</div>
+            </template>
+          </el-table-column>
+        </template>
       </el-table>
     </div>
   </div>
