d429a5dc656f5bf9cd86cf370b1f07bf151742cc
Li Yuanchao <lyc163mail@163.com>
2021-12-14 16:05:59 +0800

    修改图表以适应性能看板的需求
    Signed-off-by: Li Yuanchao <lyc163mail@163.com>

--- src/views/performanceresult/performanceresult.vue
+++ src/views/performanceresult/performanceresult.vue
@@ -28,14 +28,35 @@
         </el-form-item>
 
         <el-form-item class="confirm">
-          <el-button @click="getEcharts">确定</el-button>
+          <el-button @click="getTable">确定</el-button>
         </el-form-item>
         <el-form-item class="example">
           <el-button @click="showExample">Example</el-button>
         </el-form-item>
       </el-form>
     </div>
-    <div id="container" class="containers"></div>
+    <div id="container" class="containers">
+      <div v-for="(t_item, t_index) in table_data" :key="t_index">
+        <el-table :data="t_item.data" border style="width: 1500px">
+          <el-table-column
+            v-for="(item, index) in t_item.header"
+            :key="index"
+            :label="item"
+            :prop="item"
+          ></el-table-column>
+        </el-table>
+
+        <div
+          :id="`average_${t_item.title.replace('.', '_')}_${t_index}`"
+          :style="{ width: '100%', height: '600px' }"
+        ></div>
+
+        <div
+          :id="`change_${t_item.title.replace('.', '_')}_${t_index}`"
+          :style="{ width: '100%', height: '600px' }"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -59,6 +80,8 @@ export default {
         series: [],
         x_params: [],
       },
+      table_data: [],
+      echart_data: [],
     };
   },
   methods: {
@@ -79,7 +102,11 @@ export default {
         }
       }
 
-      this.QueryData.metrics = this.metrics.split(" ").join("").split(";");
+      if (this.metrics == "") {
+        this.QueryData.metrics = [];
+      } else {
+        this.QueryData.metrics = this.metrics.split(" ").join("").split(";");
+      }
       this.QueryData.x_params = this.x_params.split(" ").join("").split(";");
 
       tmp = this.series.split(" ").join("").split(";");
@@ -158,14 +185,14 @@ export default {
         ],
       };
     },
-    renderSeries(sourceData) {
+    renderSeries(sourceData, echart_type) {
       //描绘折线图+方差图所需的参数
       var series = [];
-      for (var j = 0; j < sourceData.datas.length; j++) {
+      for (var j = 0; j < sourceData.length; j++) {
         var objLine = {
-          name: sourceData.datas[j].name,
-          type: "line",
-          data: sourceData.datas[j].data,
+          name: sourceData[j].series,
+          type: echart_type,
+          data: sourceData[j].data,
         };
         var objCustom = {
           type: "custom",
@@ -175,11 +202,6 @@ export default {
               borderType: "dotted",
             },
           },
-          renderItem: this.renderItem,
-          data: this.renderDeviationAreaArray(
-            sourceData.datas[j].data,
-            sourceData.datas[j].deviation
-          ), //返回方差数组
           z: 100,
         };
 
@@ -191,8 +213,8 @@ export default {
     },
     getSeriesNames(sourceData) {
       var s_names = [];
-      for (var i = 0; i < sourceData.datas.length; i++) {
-        s_names[i] = sourceData.datas[i].name;
+      for (var i = 0; i < sourceData.length; i++) {
+        s_names[i] = sourceData[i].series;
       }
       return s_names;
     },
@@ -201,83 +223,130 @@ export default {
       for (var i = container.children.length - 1; i >= 0; i--) {
         container.removeChild(container.children[i]);
       }
+      this.table_data = [];
     },
-    getEcharts() {
-      var container = document.getElementById("container");
-      this.echart_clean();
+    getTableData(sData) {
+      var avg_data = sData.datas.average;
+      var change_data = sData.datas.change;
+      var tData = [];
+      var tHeader = [];
 
+      tHeader.push(sData.title);
+      tHeader = tHeader.concat(avg_data[0].x_params);
+      for (var i = 0; i < avg_data.length; i++) {
+        var tmp = this.getRowData(sData.title, avg_data[i]);
+        tData.push(tmp);
+      }
+
+      for (i = 0; i < change_data.length; i++) {
+        tmp = this.getRowData(sData.title, change_data[i]);
+        tData.push(tmp);
+      }
+      this.table_data.push({
+        header: tHeader,
+        data: tData,
+        title: sData.title,
+      });
+    },
+    getRowData(title, data) {
+      var tmp = {};
+      tmp[title] = data.series;
+
+      for (var i = 0; i < data.x_params.length; i++) {
+        tmp[data.x_params[i]] = data.data[i];
+      }
+      return tmp;
+    },
+    getTable() {
       this.getQueryData();
       if (this.QueryData) {
         getPerformanceResult(this.QueryData).then((res) => {
+          this.echart_data = res;
           var sourceData = res;
+
           for (var i = 0; i < sourceData.length; i++) {
-            var el = document.createElement("div");
-            el.id = "chart" + i;
-            el.className = "chart";
-            el.style = "width:1600px;height:720px;";
-            container.appendChild(el);
-            var myChart = echarts.init(document.getElementById("chart" + i));
-
-            var option = {
-              tooltip: {
-                trigger: "axis",
-                axisPointer: {
-                  type: "shadow",
-                },
-
-                formatter(params) {
-                  var res = "<p>x_value: " + params[0].name + "</p>";
-                  var s_name;
-                  var average;
-                  var deviation;
-                  var tmp;
-
-                  for (var i = 0; i < params.length; i = i + 2) {
-                    s_name = "<p>" + params[i].seriesName + "</p>";
-                    if (params[i].data > 1000000) {
-                      tmp = params[i].data.toFixed(3);
-                    } else {
-                      tmp = params[i].data;
-                    }
-                    average = "<p>" + "average: " + tmp + "</p>";
-                    if (params[i + 1].data[1] > 1000000) {
-                      tmp = params[i + 1].data[1].toFixed(3);
-                    } else {
-                      tmp = params[i + 1].data[1];
-                    }
-                    deviation = "<p>" + "deviation: " + tmp + "</p>";
-                    res += "</br>" + s_name + average + deviation;
-                  }
-                  return res;
-                },
-              },
-              title: {
-                text: sourceData[i].title,
-              },
-              legend: {
-                data: this.getSeriesNames(sourceData[i]),
-              },
-              xAxis: {
-                data: sourceData[i].datas[0].x_params,
-              },
-              yAxis: {},
-
-              grid: {
-                left: "15%",
-              },
-            };
-            var series = this.renderSeries(sourceData[i]);
-            option.series = series;
-            option && myChart.setOption(option, true); //绘图
+            this.getTableData(sourceData[i]);
           }
+          this.sleep(100).then(() => {
+            this.get_Echarts();
+          });
         });
       }
     },
+    sleep(time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+    },
+    get_Echarts() {
+      for (var i = 0; i < this.echart_data.length; i++) {
+        var echart_title = this.echart_data[i].title;
+
+        this.drawEchart(
+          echart_title,
+          i,
+          "average",
+          "line",
+          this.echart_data[i].datas.average
+        );
+
+        this.drawEchart(
+          echart_title,
+          i,
+          "change",
+          "bar",
+          this.echart_data[i].datas.change
+        );
+      }
+    },
+    drawEchart(echart_title,index, data_type, echart_type, e_data) {
+      var echart_id = data_type + "_" + echart_title.replace(".", "_") + '_' + index;
+      var myChart = echarts.init(document.getElementById(echart_id));
+
+      var option = {
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "shadow",
+          },
+
+          formatter(params) {
+            var res = "<p>x_value: " + params[0].name + "</p>";
+            var s_name;
+            var average;
+            var tmp;
+
+            for (var i = 0; i < params.length; i++) {
+              s_name = "<p>" + params[i].seriesName + "</p>";
+              tmp = params[i].data.toFixed(4);
+              average = "<p>" + data_type + ":" + tmp + "</p>";
+              res += "</br>" + s_name + average;
+            }
+            return res;
+          },
+        },
+        title: {
+          text: echart_title,
+        },
+        legend: {
+          data: this.getSeriesNames(e_data),
+        },
+        xAxis: {
+          data: e_data[0].x_params,
+        },
+        yAxis: {},
+
+        grid: {
+          left: "15%",
+        },
+      };
+      var series = this.renderSeries(e_data, echart_type);
+      option.series = series;
+      option && myChart.setOption(option, true); //绘图
+    },
     showExample() {
-      this.filter = "suite:atomic;";
-      this.series = "os:openeuler;os:centos;";
-      this.metrics = "atomic.score";
-      this.x_params = "nr_threads";
+      this.filter = "suite:netperf;";
+      this.series = "os_version:21.09-iso;os_version:21.03-iso;";
+      this.metrics = "netperf.workload";
+      this.x_params = "send_size";
     },
   },
   mounted() {},
