684173a1e6f52d3aec626e35e5a96f6cc5c6cf25
Lu Weitao <luweitaobe@163.com>
2022-03-07 14:43:01 +0800

    modify base url for new api services
    Signed-off-by: Lu Weitao <luweitaobe@163.com>

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,5 @@
 /**
  * @file 常用文件路径
  * */
-// export const BASEURLRESULT = 'http://124.90.34.227:11300';
-export const BASEURLRESULT = 'https://api.compass-ci.openeuler.org:20007';
+export const BASEURLRESULT = 'https://api.compass-ci.openeuler.org';
 export const BASEURLTESTBOX = 'https://gitee.com/wu_fengguang/lab-z9/tree/master/hosts/';
