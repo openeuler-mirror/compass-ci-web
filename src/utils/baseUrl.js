dcf099f846188a6fb4a5a136662bf10a69bdef59
Lu Weitao <luweitaobe@163.com>
2022-03-31 23:21:27 +0800

    src/utils/baseUrl.js: change BASEURLRESULT
    Signed-off-by: Lu Weitao <luweitaobe@163.com>

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -2,5 +2,5 @@
  * @file 常用文件路径
  * */
 // export const BASEURLRESULT = 'http://124.90.34.227:11300';
-export const BASEURLRESULT = 'http://183.134.196.212:11300';
+export const BASEURLRESULT = 'http://124.160.11.58:11300';
 export const BASEURLTESTBOX = 'https://gitee.com/wu_fengguang/lab-z9/tree/master/hosts/';
