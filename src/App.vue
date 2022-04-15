ff187355c5e849ab85992b414b37aed31c52b9f6
zeroacmer <10791894@qq.com>
2022-04-15 12:50:28 +0800

    添加导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
-  <div id="app">
-    <router-view class="container" />
+  <div id="app" >
+    <div id="nav" >
+      <el-menu
+          router="true"
+          :default-active="1"
+          class="header_title"
+          mode="horizontal">
+        <el-menu-item index="1" route="/"><div ><i class="el-icon-house"></i>home</div></el-menu-item>
+        <el-menu-item index="2" route="/jobs"><div ><i class="el-icon-position"></i>jobs</div></el-menu-item>
+        <el-menu-item index="3" route="/repos"><div><i class="el-icon-star-off"></i>repos</div></el-menu-item>
+        <el-menu-item index="4" route="/oepkgs"><div ><i class="el-icon-notebook-2"></i>oepkgs</div></el-menu-item>
+        <el-menu-item index="5" route="/compare"><div ><i class="el-icon-odometer"></i>compareResult</div></el-menu-item>
+        <el-submenu index="6">
+          <template slot="title"><i class="el-icon-data-analysis"></i>summary</template>
+          <el-menu-item index="6-1" route="/job-summary">jobSummary</el-menu-item>
+          <el-menu-item index="6-2" route="/performance-result">performanceResult</el-menu-item>
+          <el-menu-item index="6-3" route="/testMatrix">testMatrix</el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </div>
+    <router-view class="container" style="margin-top: 3.7%"/>
     <Footer class="main-footer"></Footer>
   </div>
 </template>
@@ -10,7 +29,7 @@ import Footer from "@/components/Footer";
 export default {
   data() {
     return {
-      
+
     }
   },
   components: {
@@ -27,7 +46,7 @@ export default {
     },
   },
   mounted() {
-    
+
   },
   updated() {
     window.scroll(0, 0);
@@ -46,5 +65,12 @@ export default {
   @media screen and (max-width: 1000px) {
     padding-bottom: 100px;
   }
+}
+.header_title {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 1000;
+  padding-left: 9%;
 }
 </style>
